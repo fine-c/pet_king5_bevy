a64a3f76098d5184e003c4c95de3e9ff36f176f9
薄荷巧克力 <2802660864@qq.com>
2026-04-23 23:20:19 +0800

    集成bevy_ecs_ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 use bevy::{
     DefaultPlugins,
     app::{App, PluginGroup, Startup},
-    camera::Camera2d,
-    ecs::system::Commands,
+    asset::AssetServer,
+    camera::{Camera2d, OrthographicProjection, Projection},
+    ecs::system::{Commands, Res},
     image::ImagePlugin,
+    transform::components::Transform,
 };
+use bevy_ecs_ldtk::{LdtkPlugin, LdtkWorldBundle, LevelSelection};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(LdtkPlugin)
         .add_systems(Startup, setup)
+        .insert_resource(LevelSelection::index(0))
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d);
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scale: 0.5,
+            ..OrthographicProjection::default_2d()
+        }),
+        Transform::from_xyz(640.0 / 2.0, 640.0 / 2.0, 0.0),
+    ));
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("pet_king5.ldtk").into(),
+        ..Default::default()
+    });
 }
